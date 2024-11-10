a9f11e8f555b6ee5d9027f3934861047b82726f4
randikasptra <saputrarandika857@gmail.com>
2024-11-10 09:55:58 +0700

    Menyelesaikan WEB

--- js/script.js
+++ js/script.js
@@ -2,3 +2,16 @@ function toggleMenu() {
     const navbar = document.querySelector('.randi_navbar');
     navbar.classList.toggle('active');
 }
+
+let isMessageOne = true;
+
+// Fungsi untuk menampilkan switch alert
+function showSwitchAlert() {
+    if (isMessageOne) {
+        alert("Terima kasih telah bergabung dengan Moura Coffee!");
+    } else {
+        alert("Selamat! Nikmati keuntungan eksklusif sebagai member Moura Coffee.");
+    }
+    // Ganti nilai isMessageOne untuk mengubah pesan saat tombol diklik lagi
+    isMessageOne = !isMessageOne;
+}
